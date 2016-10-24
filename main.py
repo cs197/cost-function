9ccb01b49f5a2105d98ceae545933f5ad42ef42f
Brian Hill <brian@lingerhere.org>
2016-10-24 11:34:20 -0700

    My solution to the cost-function assignment.

--- main.py
+++ main.py
@@ -2,8 +2,35 @@
 from cost_function_definition import f, f_prime, f_double_prime
 
 
+# return's a new guess using Newton's method
+def newtons_method(f1, f2, guess):
+    slope = f1(guess)
+    curvature = f2(guess)
+
+    if slope == 0.0:
+        # already at a stationary point
+        return guess
+
+    if curvature == 0.0:
+        # oh-oh, no curvature, means Newton's next guess is infinitely far away
+        delta = -1.0 / slope  # the only dimensionally-correct thing to return
+    else:
+        # after dispensing with the special cases, finally apply Newton's method
+        delta = -slope / curvature
+
+    return guess + delta
+
+
 def minimize(f0, f1, f2, guess, epsilon):
-    print str(f0(guess)) + " is minimized at " + str(guess)
+    new_guess = newtons_method(f1, f2, guess)
+
+    while abs(new_guess - guess) >= epsilon:
+        # guess again
+        guess = new_guess
+        new_guess = newtons_method(f1, f2, new_guess)
+
+    print "Minimum of " + str(f0(new_guess)) + " found at " + str(new_guess)
 
 
-minimize(f, f_prime, f_double_prime, 0, 0.000001)
+if __name__ == "__main__":
+    minimize(f, f_prime, f_double_prime, 0.0, 0.000001)
--- test_newtons_method.py
+++ test_newtons_method.py
@@ -1,8 +1,26 @@
 
 import unittest
 
+from main import newtons_method
+
+
+def f0(x):
+    return 0.5 * x * x
+
+
+def f1(x):
+    return x
+
+
+# noinspection PyUnusedLocal
+def f2(x):
+    return 1.0
+
 
 class TestNewtonsMethod(unittest.TestCase):
 
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        guess = -1.0
+        new_guess = newtons_method(f1, f2, guess)
+        expected_new_guess = 0.0
+        self.assertEqual(new_guess, expected_new_guess)
